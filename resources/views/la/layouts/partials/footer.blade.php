de8d16cd868abb4e2d19a7d9589e5f9f3b7450f4
Castilho <wesley.castilho@thomsonreuters.com>
2020-04-21 13:40:31 -0300

    first commit

---
+++ resources/views/la/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+@if(!isset($no_padding))
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        Feito por <a href="https://meuapoio.com.br">Meu Apoio</a>
+    </div>
+    <strong>Copyright &copy; {{date('Y')}}
+</footer>
+@endif
